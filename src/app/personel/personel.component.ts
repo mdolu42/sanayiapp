1eb8cebcfc621fc1b1011821b2386fd3d186ba45
gcolmekci <gcolmekci@gmail.com>
2023-01-18 14:22:17 +0300

    onsekiz

--- src/app/personel/personel.component.ts
+++ src/app/personel/personel.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $:any;
 @Component({
   selector: 'app-personel',
   templateUrl: './personel.component.html',
@@ -11,5 +11,22 @@ export class PersonelComponent implements OnInit {
 
   ngOnInit() {
   }
+  showNotification(from, align){
+    const type = ['success'];
+
+    var color = Math.floor((Math.random() * 4) + 1);
+    $.notify({
+        icon: "pe-7s-gift",
+        message: "Personel Ekleme <b>Başarlı</b>"
+    },{
+        type: type['success'],
+        timer: 1000,
+        placement: {
+            from: from,
+            align: align
+        }
+    });
+}
+
 
 }
